1f4328c7db453d8820641790dfb8137847057095
Baik <bcy0729@naver.com>
2019-04-14 21:13:41 +0900

    인트로, 로그인, 홈, 사이드바 마크업 및 스타일링

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,20 @@
-
-
-
 $(function () {
     lifeon.init();
 });
 
 const lifeon = {
 
+    init : function () {
+
+        lifeon.onSearch();
+    },
+
+    onSearch : function () {
+        $('.search-group .search-box').on('click', function () {
+            $('html,body').addClass('onSearch');
+        })
+        $('.search-group .btn-reset').on('click',function () {
+            $('html,body').removeClass('onSearch');
+        })
+    },
 };
