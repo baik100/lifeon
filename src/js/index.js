334a9b3f71d244c1c0882fbbf6d130d74907e37e
Baik <bcy0729@naver.com>
2019-04-16 00:33:38 +0900

    스타일, 아파트먼트, 프리미엄, 마이페이지 마크업

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,9 @@ const lifeon = {
 
         lifeon.onSearch();
         lifeon.handleSideOpen();
+        lifeon.loadImages();
+        lifeon.fileLodaer();
+        lifeon.navTab();
     },
 
     onSearch : function () {
@@ -26,5 +29,47 @@ const lifeon = {
         $('.side-screen').on('click',function () {
             $('html,body').removeClass('is-side-open');
         })
-    }
+    },
+
+    navTab : function () {
+        const defaultIndex = 0;
+
+        $('.tab-nav').on('click',function () {
+            const self = $(this);
+            const tabContent = $('.tab-content');
+
+            self.addClass('on').siblings().removeClass('on');
+
+            // const dataPanel = $this.attr('data-panel');
+            const dataPanel = self.data('tab');
+
+            tabContent.hide();
+            $(dataPanel).show();
+        }).eq(defaultIndex).trigger('click');
+    },
+
+    loadImages : function () {
+        const $grid = $('.grid').masonry({
+            itemSelector: '.grid-item',
+            columnWidth: '.grid-item',
+            percentPosition: true,
+            horizontalOrder: true
+
+        })
+
+        $grid.imagesLoaded().progress( function() {
+            $grid.masonry('layout');
+        });
+    },
+
+    fileLodaer : function () {
+        $('#fileLoader').on('change',function () {
+            const value = $(this).val();
+            const filename = value.split('\\').pop();
+
+            $('.filename').text(filename);
+        })
+    },
+
+
 };
